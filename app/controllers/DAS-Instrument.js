6735061e39d99025aebe6414f11c89c300413e96
Guillermo Uribe <bluememon@gmail.com>
2015-03-10 00:56:57 -0600

    Agregado Instrumento DAS-A junto con su interaccion con la base de datos

---
+++ app/controllers/DAS-Instrument.js
@@ -0,0 +1,300 @@
+        $.sendDASA.addEventListener("click", function(){
+        	alert("holaMiguel!");
+        	insertData();
+        });
+        
+        $.question1.addEventListener('stop', function(e) {
+		    if(e.value < 1.5) {
+		        $.question1.value = 1;
+		    }
+		    else if(e.value < 2.5) {
+		        $.question1.value = 2;
+		    }
+		    else if(e.value < 3.5) {
+		        $.question1.value = 3;
+		    }
+		    else if(e.value < 4.5) {
+		        $.question1.value = 4;
+		    }
+		    else if(e.value < 5.5) {
+		        $.question1.value = 5;
+		    }
+		    else if(e.value < 6.5) {
+		        $.question1.value = 6;
+		    }
+		    else if(e.value < 7.5) {
+		        $.question1.value = 7;
+		    }
+		    else if(e.value < 8.5) {
+		        $.question1.value = 8;
+		    }
+		    else if(e.value < 9.5) {
+		        $.question1.value = 9;
+		    }
+		    else {
+		        $.question1.value = 10;
+		    }
+		});
+		
+		$.question2.addEventListener('stop', function(e) {
+		    if(e.value < 1.5) {
+		        $.question2.value = 1;
+		    }
+		    else if(e.value < 2.5) {
+		        $.question2.value = 2;
+		    }
+		    else if(e.value < 3.5) {
+		        $.question2.value = 3;
+		    }
+		    else if(e.value < 4.5) {
+		        $.question2.value = 4;
+		    }
+		    else if(e.value < 5.5) {
+		        $.question2.value = 5;
+		    }
+		    else if(e.value < 6.5) {
+		        $.question2.value = 6;
+		    }
+		    else if(e.value < 7.5) {
+		        $.question2.value = 7;
+		    }
+		    else if(e.value < 8.5) {
+		        $.question2.value = 8;
+		    }
+		    else if(e.value < 9.5) {
+		        $.question2.value = 9;
+		    }
+		    else {
+		        $.question2.value = 10;
+		    }
+		});
+		
+		$.question3.addEventListener('stop', function(e) {
+		    if(e.value < 1.5) {
+		        $.question3.value = 1;
+		    }
+		    else if(e.value < 2.5) {
+		        $.question3.value = 2;
+		    }
+		    else if(e.value < 3.5) {
+		        $.question3.value = 3;
+		    }
+		    else if(e.value < 4.5) {
+		        $.question3.value = 4;
+		    }
+		    else if(e.value < 5.5) {
+		        $.question3.value = 5;
+		    }
+		    else if(e.value < 6.5) {
+		        $.question3.value = 6;
+		    }
+		    else if(e.value < 7.5) {
+		        $.question3.value = 7;
+		    }
+		    else if(e.value < 8.5) {
+		        $.question3.value = 8;
+		    }
+		    else if(e.value < 9.5) {
+		        $.question3.value = 9;
+		    }
+		    else {
+		        $.question3.value = 10;
+		    }
+		});
+		
+		$.question4.addEventListener('stop', function(e) {
+		    if(e.value < 1.5) {
+		        $.question4.value = 1;
+		    }
+		    else if(e.value < 2.5) {
+		        $.question4.value = 2;
+		    }
+		    else if(e.value < 3.5) {
+		        $.question4.value = 3;
+		    }
+		    else if(e.value < 4.5) {
+		        $.question4.value = 4;
+		    }
+		    else if(e.value < 5.5) {
+		        $.question4.value = 5;
+		    }
+		    else if(e.value < 6.5) {
+		        $.question4.value = 6;
+		    }
+		    else if(e.value < 7.5) {
+		        $.question4.value = 7;
+		    }
+		    else if(e.value < 8.5) {
+		        $.question4.value = 8;
+		    }
+		    else if(e.value < 9.5) {
+		        $.question4.value = 9;
+		    }
+		    else {
+		        $.question4.value = 10;
+		    }
+		});
+        
+        $.question5.addEventListener('stop', function(e) {
+		    if(e.value < 1.5) {
+		        $.question5.value = 1;
+		    }
+		    else if(e.value < 2.5) {
+		        $.question5.value = 2;
+		    }
+		    else if(e.value < 3.5) {
+		        $.question5.value = 3;
+		    }
+		    else if(e.value < 4.5) {
+		        $.question5.value = 4;
+		    }
+		    else if(e.value < 5.5) {
+		        $.question5.value = 5;
+		    }
+		    else if(e.value < 6.5) {
+		        $.question5.value = 6;
+		    }
+		    else if(e.value < 7.5) {
+		        $.question5.value = 7;
+		    }
+		    else if(e.value < 8.5) {
+		        $.question5.value = 8;
+		    }
+		    else if(e.value < 9.5) {
+		        $.question5.value = 9;
+		    }
+		    else {
+		        $.question5.value = 10;
+		    }
+		});
+        
+        $.question6.addEventListener('stop', function(e) {
+		    if(e.value < 1.5) {
+		         $.question6.value = 1;
+		    }
+		    else if(e.value < 2.5) {
+		         $.question6.value = 2;
+		    }
+		    else if(e.value < 3.5) {
+		         $.question6.value = 3;
+		    }
+		    else if(e.value < 4.5) {
+		         $.question6.value = 4;
+		    }
+		    else if(e.value < 5.5) {
+		         $.question6.value = 5;
+		    }
+		    else if(e.value < 6.5) {
+		         $.question6.value = 6;
+		    }
+		    else if(e.value < 7.5) {
+		         $.question6.value = 7;
+		    }
+		    else if(e.value < 8.5) {
+		         $.question6.value = 8;
+		    }
+		    else if(e.value < 9.5) {
+		         $.question6.value = 9;
+		    }
+		    else {
+		         $.question6.value = 10;
+		    }
+		});
+        
+        $.question7.addEventListener('stop', function(e) {
+		    if(e.value < 1.5) {
+		        $.question7.value = 1;
+		    }
+		    else if(e.value < 2.5) {
+		        $.question7.value = 2;
+		    }
+		    else if(e.value < 3.5) {
+		        $.question7.value = 3;
+		    }
+		    else if(e.value < 4.5) {
+		        $.question7.value = 4;
+		    }
+		    else if(e.value < 5.5) {
+		        $.question7.value = 5;
+		    }
+		    else if(e.value < 6.5) {
+		        $.question7.value = 6;
+		    }
+		    else if(e.value < 7.5) {
+		        $.question7.value = 7;
+		    }
+		    else if(e.value < 8.5) {
+		        $.question7.value = 8;
+		    }
+		    else if(e.value < 9.5) {
+		        $.question7.value = 9;
+		    }
+		    else {
+		        $.question7.value = 10;
+		    }
+		});
+        
+        $.question8.addEventListener('stop', function(e) {
+		    if(e.value < 1.5) {
+		        $.question8.value = 1;
+		    }
+		    else if(e.value < 2.5) {
+		        $.question8.value = 2;
+		    }
+		    else if(e.value < 3.5) {
+		        $.question8.value = 3;
+		    }
+		    else if(e.value < 4.5) {
+		        $.question8.value = 4;
+		    }
+		    else if(e.value < 5.5) {
+		        $.question8.value = 5;
+		    }
+		    else if(e.value < 6.5) {
+		        $.question8.value = 6;
+		    }
+		    else if(e.value < 7.5) {
+		        $.question8.value = 7;
+		    }
+		    else if(e.value < 8.5) {
+		        $.question8.value = 8;
+		    }
+		    else if(e.value < 9.5) {
+		        $.question8.value = 9;
+		    }
+		    else {
+		        $.question8.value = 10;
+		    }
+		});
+        
+        function insertData(){ 
+              //if there is something in the textbox 
+              if($.question1.value != "" && $.question1.value != null){ 
+                     var request = Ti.Network.createHTTPClient({ 
+                  onload:alert("Your chore has been submitted"), 
+                  onerror: function(e){ 
+                      Ti.API.debug(e.error); 
+                      alert('There was an error during the conexion'); 
+                  }, 
+                  timeout:1000, 
+                     });    
+//Request the data from the web service, Here you have to change it for your local ip 
+                     request.open("POST","http://app.bluecoreservices.com/webservices/addDAS-A.php"); 
+                     var params = ({
+                     	"id": "1337",
+                     	"question1": $.question1.value,
+                     	"question2": $.question2.value,
+                     	"question3": $.question3.value,
+                     	"question4": $.question4.value,
+                     	"question5": $.question5.value,
+                     	"question6": $.question6.value,
+                     	"question7": $.question7.value,
+                     	"question8": $.question8.value,
+                     	});  
+                   alert(params);
+                  request.send(params); 
+              } 
+              else{ 
+                     alert("Please write something in the textbox"); 
+              }               
+       };   
