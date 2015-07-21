487948c47d1d839fdbf6fdcc2accd599eeacf61f
Guillermo Uribe <bluememon@gmail.com>
2015-07-20 20:50:38 -0500

    Agregado ultimas categorias a la app

--- app/controllers/categoList.js
+++ app/controllers/categoList.js
@@ -6,6 +6,7 @@ var dataArrayCatego = [];
 
 var buttonToggle = false;
 
+getTodoList(idPaciente);
 $.activityIndicator.show();
 loadData();
 
@@ -14,7 +15,7 @@ function loadData(){
 	 onerror: function(e){ 
 	       Ti.API.debug(e.error); 
 	       alert('There was an error during the connection'); 
-	       $.connectionError.show();
+	       //$.connectionError.show();
 	         }, 
 	      timeout:3000, 
 	  });                      
@@ -134,3 +135,122 @@ function agregarColor(resultado) {
 		return "#66CCFF";
 	}
 };
+
+function getTodoList (idPatient) { 
+   //function to use HTTP to connect to a web server and transfer the data. 
+      var sendit = Ti.Network.createHTTPClient({ 
+             onerror: function(e){ 
+                   Ti.API.debug(e.error); 
+                   alert('There was an error during the connection'); 
+                   $.connectionError.show();
+             }, 
+          timeout:3000, 
+      });                      
+      //Here you have to change it for your local ip 
+      sendit.open('POST', 'http://app.bluecoreservices.com/webservices/ListCatego.php');
+      var params = ({
+      	"idPaciente": idPatient,
+      });
+      sendit.send(params); 
+      //Function to be called upon a successful response 
+      sendit.onload = function(){ 
+             var json = JSON.parse(this.responseText); 
+             var json = json.DasList; 
+             //if the database is empty show an alert 
+             if(json.length == 0){ 
+             	$.noInfoView.show();       
+             }                      
+             //Emptying the data to refresh the view 
+             dataArray = []; 
+             
+             //we add the separator
+             var separator = Ti.UI.createView({
+		        width: Titanium.UI.FILL,
+		        height:1,
+		        backgroundColor:'#999999',
+		        bottom:0,
+		 
+		    });
+		    //we add the first separator
+		    $.DASCatego.add(separator);
+                                  
+             //Insert the JSON data to the table view 
+             for( var i=0; i<json.length; i++){ 
+             	
+                 var row = Titanium.UI.createView({
+                 		className: 'elementRow',
+                 		layout: 'horizontal',
+						height: Titanium.UI.SIZE,
+						width: Titanium.UI.FILL
+         		 });                                     		
+                 
+                 var viewResult = Titanium.UI.createView({
+                 	className: 'rowResult',
+                 	height: Titanium.UI.SIZE,
+					width: Titanium.UI.SIZE,
+					bubbleParent: true,
+					layout: 'composite'
+                 });
+                 
+                 var viewResultColor = Titanium.UI.createView({
+                 	className: 'rowResult',
+                 	height: '60sp',
+					width: '60sp',
+					borderRadius: '30sp',
+					backgroundColor: agregarColor(json[i].total),
+					top: '10sp',
+					bottom: '10sp',
+					left: '10sp',
+					right: '10sp',
+					bubbleParent: true,
+					layout: 'composite'
+                 });
+                 
+                 var viewDate = Titanium.UI.createView({
+                 	className: 'rowDate',
+                 	height: Titanium.UI.SIZE,
+					width: Titanium.UI.SIZE,
+					left: "10sp",
+					bubbleParent: true
+                 });
+                 
+                 var resultLabel = Titanium.UI.createLabel({
+                 	text: Math.round(json[i].severidad),
+                 	font: {
+                 		fontSize: '20sp'
+                 	},
+                 	color: '#FFFFFF'
+                 });
+                 
+                  var dateLabel = Titanium.UI.createLabel({
+                 	text: json[i].nombre,
+                 	font: {
+                 		fontSize: '20sp'
+                 	},
+                 	color: '#000000'
+                 });
+                 
+                 var separator = Ti.UI.createView({
+			        width: Titanium.UI.FILL,
+			        height:1,
+			        backgroundColor:'#999999',
+			        bottom:0,
+			 
+			    });
+                 
+                 viewResult.add(resultLabel);
+                 viewDate.add(dateLabel);
+                 
+                 viewResultColor.add(viewResult);
+                 
+                 row.add(viewResultColor);
+                 row.add(viewDate);                     
+                           
+                 //dataArray.push(row);
+                 $.DASCatego.add(row);
+                 $.DASCatego.add(separator);                 
+             }; 
+             $.activityIndicator.hide();                    
+             //$.DASListas.add(dataArray);                            
+           }; 
+   };
